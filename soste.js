643e14e11fa0c7c66eab63b13794fb208cd49588
Alessandro <andro.mieli2001@gmail.com>
2021-04-05 23:50:32 +0200

    Demo v1.0

---
+++ soste.js
@@ -0,0 +1,382 @@
+var soste = [
+    {
+        id: "1",
+        nome: "GIMMA  2"
+    },
+    {
+        id: "2",
+        nome: "VIALE PASTEUR  61/63"
+    },
+    {
+        id: "3",
+        nome: "MONTAGNOLA  8"
+    },
+    {
+        id: "4",
+        nome: "TORREVECCHIA"
+    },
+    {
+        id: "5",
+        nome: "SCARPANTO  4"
+    },
+    {
+        id: "6",
+        nome: "BARLETTA"
+    },
+    {
+        id: "7",
+        nome: "GENTILE DA MOGLIANO"
+    },
+    {
+        id: "8",
+        nome: "S.G.DI D. PIAZZISTA"
+    },
+    {
+        id: "9",
+        nome: "SATRICO"
+    },
+    {
+        id: "10",
+        nome: "MARCONI  2"
+    },
+    {
+        id: "11",
+        nome: "COLLI  ALBANI  METRO"
+    },
+    {
+        id: "12",
+        nome: "PIAZZALE  DEGLI  EROI"
+    },
+    {
+        id: "13",
+        nome: "NOBILIORE"
+    },
+    {
+        id: "14",
+        nome: "GIURECONSULTI"
+    },
+    {
+        id: "15",
+        nome: "MONTAGNOLA  13"
+    },
+    {
+        id: "16",
+        nome: "SPARTACO "
+    },
+    {
+        id: "17",
+        nome: "GERMANICO"
+    },
+    {
+        id: "18",
+        nome: "PRIMAVERE"
+    },
+    {
+        id: "19",
+        nome: "CARDANO"
+    },
+    {
+        id: "20",
+        nome: "LABIENO  5"
+    },
+    {
+        id: "21",
+        nome: "SAGRATO"
+    },
+    {
+        id: "22",
+        nome: "TITO  SPERI  VECCHIA"
+    },
+    {
+        id: "23",
+        nome: "MAMMOLO"
+    },
+    {
+        id: "24",
+        nome: "ORVIETO"
+    },
+    {
+        id: "25",
+        nome: "PZZA.  CINA  POST. 5"
+    },
+    {
+        id: "26",
+        nome: "VIA  SANSOTTA  POST. 1"
+    },
+    {
+        id: "27",
+        nome: "VIA  IGEA"
+    },
+    {
+        id: "28",
+        nome: "GIMMA 1"
+    },
+    {
+        id: "29",
+        nome: "QUADRARO  3°"
+    },
+    {
+        id: "30",
+        nome: "BALBO"
+    },
+    {
+        id: "31",
+        nome: "ENEA"
+    },
+    {
+        id: "32",
+        nome: "SABOTINO"
+    },
+    {
+        id: "33",
+        nome: "SBIRRO STALLO 15"
+    },
+    {
+        id: "34",
+        nome: "OSCI"
+    },
+    {
+        id: "35",
+        nome: "SCIPIONI"
+    },
+    {
+        id: "36",
+        nome: "PONTE  MILVIO "
+    },
+    {
+        id: "37",
+        nome: "GORGONA 3"
+    },
+    {
+        id: "38",
+        nome: "CHIANA"
+    },
+    {
+        id: "39",
+        nome: "TRASTEVERE SURROPAGO"
+    },
+    {
+        id: "40",
+        nome: "PAVIA"
+    },
+    {
+        id: "41",
+        nome: "METAURO "
+    },
+    {
+        id: "42",
+        nome: "QUADRARO 2°"
+    },
+    {
+        id: "43",
+        nome: "PAOLO  EMILIO"
+    },
+    {
+        id: "44",
+        nome: "SBIRRO  STALLO  12"
+    },
+    {
+        id: "45",
+        nome: "SOMALIA  FARMACIA"
+    },
+    {
+        id: "46",
+        nome: "MONTEBELLO"
+    },
+    {
+        id: "47",
+        nome: "SACCO  E  VANZETTI"
+    },
+    {
+        id: "48",
+        nome: "PIRAMIDE  VECCHIA"
+    },
+    {
+        id: "49",
+        nome: "QUADRARO  VECCHIA "
+    },
+    {
+        id: "50",
+        nome: "CASILINA"
+    },
+    {
+        id: "51",
+        nome: "CHERSO"
+    },
+    {
+        id: "52",
+        nome: "TRASTEVERE  FRONTONI"
+    },
+    {
+        id: "53",
+        nome: "PIRAMIDE NUOVA"
+    },
+    {
+        id: "54",
+        nome: "FILIPPO  MEDA  STALLO  9"
+    },
+    {
+        id: "55",
+        nome: "FERRARI"
+    },
+    {
+        id: "56",
+        nome: "RAPPINI"
+    },
+    {
+        id: "57",
+        nome: "RIALTO"
+    },
+    {
+        id: "58",
+        nome: "FRANKLIN"
+    },
+    {
+        id: "59",
+        nome: "ANAGNINA"
+    },
+    {
+        id: "60",
+        nome: "S. MARIA  MAGGIORE"
+    },
+    {
+        id: "61",
+        nome: "PINO  1"
+    },
+    {
+        id: "62",
+        nome: "VIA  NAPOLI  CIV. 2"
+    },
+    {
+        id: "63",
+        nome: "SAN SILVERIO"
+    },
+    {
+        id: "64",
+        nome: "OVIDIO"
+    },
+    {
+        id: "65",
+        nome: "MONTAGNOLA  2"
+    },
+    {
+        id: "66",
+        nome: "TORPIGNATTARA"
+    },
+    {
+        id: "67",
+        nome: "CASALBERTONE"
+    },
+    {
+        id: "68",
+        nome: "BOCCEA  C.E.M."
+    },
+    {
+        id: "69",
+        nome: "VALMELAINA  NUOVA"
+    },
+    {
+        id: "70",
+        nome: "PRIMATI  SPORTIVI  P. 6"
+    },
+    {
+        id: "71",
+        nome: "IONIO"
+    },
+    {
+        id: "72",
+        nome: "CAPO  PASSERO"
+    },
+    {
+        id: "73",
+        nome: "VIA  NAPOLI  ST.  4"
+    },
+    {
+        id: "74",
+        nome: "VALDOSSOLA"
+    },
+    {
+        id: "75",
+        nome: "TITO  SPERI  CESSI"
+    },
+    {
+        id: "76",
+        nome: "MAGLIANA"
+    },
+    {
+        id: "77",
+        nome: "SBIRRO  STALLO 10"
+    },
+    {
+        id: "78",
+        nome: "SOMALIA  BANCA"
+    },
+    {
+        id: "79",
+        nome: "SILLA"
+    },
+    {
+        id: "80",
+        nome: "QUADRARO  4°"
+    },
+    {
+        id: "81",
+        nome: "MONTAGNOLA  5"
+    },
+    {
+        id: "82",
+        nome: "OSTIENSE  313 "
+    },
+    {
+        id: "83",
+        nome: "CIPRO"
+    },
+    {
+        id: "84",
+        nome: "PIGNETO"
+    },
+    {
+        id: "85",
+        nome: "M.TO  SPINACETO  P. 8"
+    },
+    {
+        id: "86",
+        nome: "GALVANI "
+    },
+    {
+        id: "87",
+        nome: "PINO  2"
+    },
+    {
+        id: "88",
+        nome: "TRIONFALE  57"
+    },
+    {
+        id: "89",
+        nome: "MARCONI  11-12"
+    },
+    {
+        id: "90",
+        nome: "SBIRRO  STALLO  7"
+    },
+    {
+        id: "91",
+        nome: "LABIENO  12"
+    },
+    {
+        id: "92",
+        nome: "NICOLINI"
+    },
+    {
+        id: "93",
+        nome: "PRIMATI  SPORTIVI  P. 9"
+    },
+    {
+        id: "94",
+        nome: "URBANO  2°"
+    },
+    {
+        id: "95",
+        nome: "VIA  NAPOLI  ST.  5"
+    }
+]
