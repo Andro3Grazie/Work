57842938690c08838e023092a99a282fe96a36f7
Alessandro <andro.mieli2001@gmail.com>
2021-04-07 13:15:29 +0200

    Fix v1.8
    - Adding Favicon
    - Adding folders to have a more clean work

---
+++ scripts/nominativi.js
@@ -0,0 +1,382 @@
+var nominativi = [
+    {
+        id: "1",
+        nome: "bigonzi f/MADBAR SHAHJAHAN"
+    },
+    {
+        id: "2",
+        nome: "trifirò a/SHAHADAT HOSSAIN"
+    },
+    {
+        id: "3",
+        nome: "EFRATI  FORTUNATA"
+    },
+    {
+        id: "4",
+        nome: "MARIKA "
+    },
+    {
+        id: "5",
+        nome: "MORESCO  FABIO"
+    },
+    {
+        id: "6",
+        nome: "AMENDOLA  ROLANDO"
+    },
+    {
+        id: "7",
+        nome: "sannino g/ YOUNIS"
+    },
+    {
+        id: "8",
+        nome: "MARCO  EGIZIANO"
+    },
+    {
+        id: "9",
+        nome: "LANGELLA  AURORA"
+    },
+    {
+        id: "10",
+        nome: "di cori sett/ AMIN  SHAIM"
+    },
+    {
+        id: "11",
+        nome: "liu min/SHAFIKUL ISLAM"
+    },
+    {
+        id: "12",
+        nome: "moriani/AMED NABIL"
+    },
+    {
+        id: "13",
+        nome: "GRECO  NADIA"
+    },
+    {
+        id: "14",
+        nome: "sberlo luana / "
+    },
+    {
+        id: "15",
+        nome: "moscati g/ISLAM RANA"
+    },
+    {
+        id: "16",
+        nome: "palombini a/AMED SHALIN"
+    },
+    {
+        id: "17",
+        nome: "zarfati pace/"
+    },
+    {
+        id: "18",
+        nome: "bigonzi r/MADBAR SHAHJAHAN"
+    },
+    {
+        id: "19",
+        nome: "di segni mario/SHAIN AMIN"
+    },
+    {
+        id: "20",
+        nome: "rotella ivana/JAMAN AKON"
+    },
+    {
+        id: "21",
+        nome: "annibballi/KABIR HUMAYUN"
+    },
+    {
+        id: "22",
+        nome: "TERRACINA ALB mezzogio"
+    },
+    {
+        id: "23",
+        nome: "fiorani e/LANGELLA  DINO"
+    },
+    {
+        id: "24",
+        nome: "caroselli nardocci/ JALAL"
+    },
+    {
+        id: "25",
+        nome: "vivanti fiorella/ "
+    },
+    {
+        id: "26",
+        nome: "LANGELLA  CIRO"
+    },
+    {
+        id: "27",
+        nome: "LAUTERI  MASSIMILIANO"
+    },
+    {
+        id: "28",
+        nome: "SPAGNOLETTO  FABIO"
+    },
+    {
+        id: "29",
+        nome: "CHILELLI  ROBERTO"
+    },
+    {
+        id: "30",
+        nome: "jim niao/HOSSAIN MOSHARAF"
+    },
+    {
+        id: "31",
+        nome: "ROSSI  LUCIANO"
+    },
+    {
+        id: "32",
+        nome: "tagliacozzo e/AMIRUL ISLAM"
+    },
+    {
+        id: "33",
+        nome: "Di veroli f./BONDI  DAVID"
+    },
+    {
+        id: "34",
+        nome: "di porto s./HOSSAIN BILLAL"
+    },
+    {
+        id: "35",
+        nome: "la vincenzo srl/HOQUE ZAIRUR"
+    },
+    {
+        id: "36",
+        nome: "GALLI   IVANO  MASSIMO"
+    },
+    {
+        id: "37",
+        nome: "PERUGIA  LELLO"
+    },
+    {
+        id: "38",
+        nome: "cialini e/HOSSAIN JOSY"
+    },
+    {
+        id: "39",
+        nome: "kershbaumer r/FARUK  KAWSAR"
+    },
+    {
+        id: "40",
+        nome: "MIELI  FABIO"
+    },
+    {
+        id: "41",
+        nome: "STORTI  FEDERICO"
+    },
+    {
+        id: "42",
+        nome: "PATITUCCI  ALFREDO"
+    },
+    {
+        id: "43",
+        nome: "terracina alberto"
+    },
+    {
+        id: "44",
+        nome: "langella/RONDINONE CLARA"
+    },
+    {
+        id: "45",
+        nome: "di castro/PIAZZA CLAUDIO"
+    },
+    {
+        id: "46",
+        nome: "moresco/EL HAKIMY ABDEL"
+    },
+    {
+        id: "47",
+        nome: "sed piazza p/FARID AHMED"
+    },
+    {
+        id: "48",
+        nome: "proietti r/EL HAKIMY DRISS"
+    },
+    {
+        id: "49",
+        nome: "CAMPAGNANO  MAURIZIO"
+    },
+    {
+        id: "50",
+        nome: "piattelli m/KHAN SHAHEEN"
+    },
+    {
+        id: "51",
+        nome: "MAGNI  ROBERTO"
+    },
+    {
+        id: "52",
+        nome: "piazza c/SHAHADAT HOSSAIN "
+    },
+    {
+        id: "53",
+        nome: "DUREGHELLO  EMANUELE"
+    },
+    {
+        id: "54",
+        nome: "sed g/MADBAR SHEHJAHAN"
+    },
+    {
+        id: "55",
+        nome: "CAMBI  LUCIANO "
+    },
+    {
+        id: "56",
+        nome: "cinti carla/ISLAM RAFIQUL"
+    },
+    {
+        id: "57",
+        nome: "trifirò a./ ZAID"
+    },
+    {
+        id: "58",
+        nome: "calzetta/HAQUE ANWARUAL"
+    },
+    {
+        id: "59",
+        nome: "di porto s/LANGELLA DINO"
+    },
+    {
+        id: "60",
+        nome: "AMENDOLA  ENZO"
+    },
+    {
+        id: "61",
+        nome: "terracina r/ELIAS MOHAMED"
+    },
+    {
+        id: "62",
+        nome: "cabibbo c/KHAN SHAHEEN"
+    },
+    {
+        id: "63",
+        nome: "del monte m/ABDUS SALAM"
+    },
+    {
+        id: "64",
+        nome: "DI  VEROLI  SANDRO"
+    },
+    {
+        id: "65",
+        nome: "terracina r/AMHED ROSTAQUE"
+    },
+    {
+        id: "66",
+        nome: "sed piaaza f/UDDIN SARUAL"
+    },
+    {
+        id: "67",
+        nome: "NICOLA"
+    },
+    {
+        id: "68",
+        nome: "ANTICOLI ALBERTO MICIONE"
+    },
+    {
+        id: "69",
+        nome: "AMATI  MICHELE"
+    },
+    {
+        id: "70",
+        nome: "efrati a./ISLAM MAINUL"
+    },
+    {
+        id: "71",
+        nome: "ANTICOLI  SETTIMIO"
+    },
+    {
+        id: "72",
+        nome: "MOSCATO  LELLO"
+    },
+    {
+        id: "73",
+        nome: "rossi e/KALAM ABBUL"
+    },
+    {
+        id: "74",
+        nome: "CIURLEO  MAURO"
+    },
+    {
+        id: "75",
+        nome: "di veroli m/LANGELLA DINO"
+    },
+    {
+        id: "76",
+        nome: "sannino g/AMINUR RAHAMAN"
+    },
+    {
+        id: "77",
+        nome: "AMATI  CESARE"
+    },
+    {
+        id: "78",
+        nome: "ABOAF ACHILLE"
+    },
+    {
+        id: "79",
+        nome: "DI  PORTO  ROBERTO"
+    },
+    {
+        id: "80",
+        nome: "ASSAD AIAN YOUNIS"
+    },
+    {
+        id: "81",
+        nome: "VENEZIANO  MARCO"
+    },
+    {
+        id: "82",
+        nome: "zarfati a/HASSAN KAMRUL"
+    },
+    {
+        id: "83",
+        nome: "MORESCO  FABIO"
+    },
+    {
+        id: "84",
+        nome: "pavoncello c/MADBAR SHA"
+    },
+    {
+        id: "85",
+        nome: "CAIAZZO  MARILENA"
+    },
+    {
+        id: "86",
+        nome: "di veroli gra/MIA MD TARA"
+    },
+    {
+        id: "87",
+        nome: "ascoli/DI VEROLI FABRIZIO"
+    },
+    {
+        id: "88",
+        nome: "di porto m/MAINUL ISLAM"
+    },
+    {
+        id: "89",
+        nome: "diletti/ROSATELLI ALBERTO"
+    },
+    {
+        id: "90",
+        nome: "dureghello e./SED  LELLO"
+    },
+    {
+        id: "91",
+        nome: "LANGELLA  DINO"
+    },
+    {
+        id: "92",
+        nome: "di porto rossana/JONI M.A."
+    },
+    {
+        id: "93",
+        nome: "rossi roberta/ZAID"
+    },
+    {
+        id: "94",
+        nome: "del monte l/AMIN  SHAIM"
+    },
+    {
+        id: "95",
+        nome: "DI  VEROLI  EUGENIO"
+    }
+]
