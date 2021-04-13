901c5cb8ffa1489facd18bc1c7e657ae846c5b6a
Alessandro <andro.mieli2001@gmail.com>
2021-04-13 09:55:10 +0200

    Fix v1.12.1
    - Save the previous search result and add clear button under it (maybe re-diseng it -> | clear btn | new search btn |)
    - Tidy up code (Javascript pages) pt.2
    - Removing domeniche's page
    - Fixing search system -> not the best way but it work (the search start from the last word if it can not find anything)
        - Adding search by number

--- scripts/funzioni.js
+++ scripts/funzioni.js
@@ -3,7 +3,8 @@ function main() {
     controllaTurno();
     stampaCalendario();
     controlloCarosello();
-    // se è un giorno festivo calcola lo stesso ma stampa come `active` del carousel che si sta sotto le coperte 
+
+    // se è un giorno festivo calcola lo stesso ma stampa come `active` del carousel che si sta sotto le coperte [DA_RIFARE]
     if ( dataOggi.getDay() == 0 || (feste.includes( ("0" + dataOggi.getDate()).slice(-2) + "/" + ("0" + (dataOggi.getMonth() + 1)).slice(-2) ) )) { // Se non è domenica   
         
         // console.log('Domenica o festivo');
@@ -23,6 +24,8 @@ function controllaTurno() {
     var dataPrimoGiorno = new Date(primoGiorno); // Imposta primoGiorno come data
     var dataOggi = new Date();
     var giorniLavorativi = 0;
+
+    // dataOggi.setDate(dataOggi.getDate() - 2); // Imposta il giorno a domenica
     
     while ( (dataOggi.getDate() >= dataPrimoGiorno.getDate() && (dataOggi.getMonth() + 1) == (dataPrimoGiorno.getMonth() + 1)) ) {
       
@@ -146,8 +149,8 @@ function controlloCarosello() {
     carouselTurno.addEventListener('slide.bs.carousel', function (val) {
         // Con il carousel si sposta anche il tunro lo cambia (se il carousel va a destra aggiungi 1 | se va a sinistra sottrai uno )
         if (val.direction == 'right') { // Se va a destra
-            // e se è minore di 95, altrimenti ricomincia da 0
-            if (turno["id"] < 95) {        
+            // e se è minore di 95 (nTurni), altrimenti ricomincia da 0
+            if (turno["id"] < nTurni) {        
                 turno["id"] = soste[(turno["id"] - 1) + 1]["id"];
                 turno["nome"] = soste[turno["id"] - 1]["nome"];
             }
@@ -157,15 +160,207 @@ function controlloCarosello() {
             }
         }
         else { // Se va a sinistra
-            // e finché è minore di 1, altrimenti ricomincia da 95
+            // e finché è minore di 1, altrimenti ricomincia da 95 (nTurni)
             if (turno["id"] > 1) {
                 turno["id"] = soste[(turno["id"] - 1) - 1]["id"];
                 turno["nome"] = soste[turno["id"] - 1]["nome"];
             } else {
-                turno["id"] = soste[95 - 1]["id"];
-                turno["nome"] = soste[95 - 1]["nome"];
+                turno["id"] = soste[nTurni - 1]["id"];
+                turno["nome"] = soste[nTurni - 1]["nome"];
             }
         }
         // console.log(turno, soste[turno["id"] - 1]["id"]);
     });
+}
+
+// Mostra il nome di chi sta a quella sosta
+function dimmiChi(id, nome) { // Fanno riferimento alle informazioni della sosta dove voglio andare
+
+    chiudiRisultatiRicerca();
+    var idNominativo = 0;
+
+    idNominativo = idTurno - (turno["id"] - id);
+    if (idNominativo > nNominativi)
+        idNominativo -= nNominativi;
+
+    $("#inputSoste").prop("value", nome).prop("disabled", true);
+    $("#risultato").html(`
+        <div class="card">
+            <div class="card-body" >
+               ${nominativi[idNominativo - 1]["nome"]}
+            </div>
+        </div>
+    `);
+}
+// Mostra la sosta dove sta quel nominativo
+function dimmiDove(id, nome) {
+
+    chiudiRisultatiRicerca();
+    var idSoste = 0;
+
+    idSoste = turno["id"] - ( idTurno - id );
+    if ( idSoste < 1 )
+        idSoste += nTurni;
+
+    $("#inputSoste").prop("value", nome).prop("disabled", true);
+    $("#risultato").html(`
+        <div class="card">
+            <div class="card-body" >
+            ${soste[idSoste - 1]["nome"]}
+            </div>
+        </div>
+    `);
+}
+// Se una riceca non da nessun risultato
+function nessunRisultato(val) {
+    $(val).append(`
+        <div class="card">
+            <div class="card-body text-center">
+                <p class="display-2">(&#62;_&#60;)</p>
+                <p class="h5">La ricerca non ha prodotto alcun risultato</p>
+            </div>
+        </div> 
+    `);
+}
+// Mostra soste o nominativi
+function mostraRisultati(val, info, i, turno) {
+
+    if (turno) {     
+        $(val).append(`
+            <div class="card" onclick="impostaApplicaSosta(${soste[i]["id"]});">
+                <div class="card-body">
+                    <div class="row">                    
+                        <div class="col-2">
+                            ${soste[i]["id"]}
+                        </div>
+                        <div class="col-auto">
+                            ${soste[i]["nome"]}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+    }
+    else if (info == 'soste') {
+        $(val).append(`
+            <div class="card" onclick="dimmiChi(${soste[i]["id"]}, '${soste[i]["nome"]}');">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-2">
+                            ${soste[i]["id"]}
+                        </div>
+                        <div class="col-auto">
+                            ${soste[i]["nome"]}
+                        </div>
+                    </div>
+                </div>
+            </div>       
+        `);
+    }
+    else if (info == 'nominativi') {
+        $(val).append(`
+            <div class="card" onclick="dimmiDove(${nominativi[i]["id"]}, '${nominativi[i]["nome"]}');">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-2">
+                            ${nominativi[i]["id"]}
+                        </div>
+                        <div class="col-auto">
+                            ${nominativi[i]["nome"]}
+                        </div>
+                    </div>
+                </div>
+            </div>       
+        `);
+    }      
+}
+
+// Imposta la sosta scelta
+function impostaApplicaSosta(id) {
+    turno["id"] = soste[id - 1]["id"];
+    turno["nome"] = soste[id - 1]["nome"];
+    stampaCalendario();
+    $('#nuovaRicerca').hide();
+    $('#risultato').hide();
+    $("#inputSoste").prop("value", '').prop("disabled", false);
+    $('#impostaSosteModal').modal('hide');
+}
+// Sistema di ricerca per cambiare la sosta
+function applicaSosta() {
+    var modal = new bootstrap.Modal(document.getElementById('impostaSosteModal'));
+    modal.toggle();
+    $('#inputImpostaSoste').focus();
+    // Prendere gli input da tastiera per la ricerca live
+    $('#inputImpostaSoste').bind('click keyup', function () {
+        $("#impostaListaSosta").html('');
+        var k = 0;
+        var nada = true;
+        for (let i = 0; i < soste.length && k < 2; i++) {
+            // Risultati live
+            if ( (soste[i]["nome"].toUpperCase().replace(/\s+/g, " ").trim().includes($(this).val().toUpperCase().replace(/\s+/g, " ").trim()) || ("0" + soste[i]["id"].trim()).slice(-2).includes(("0" + $(this).val().trim()).slice(-2)) ) && $(this).val() != '' )  {
+                nada = false;
+                k +=1; 
+                mostraRisultati('#impostaListaSosta', 'soste', i, true);
+            }
+        }
+        if (nada && $(this).val() != '') { // La ricerca non ha prodotto risultati :(
+            nessunRisultato('#impostaListaSosta');
+        }
+    });
+}
+// Sistema di ricerca
+function risultatiRicerca(val) {
+    var nada = true;
+    for (let i = 0; i < soste.length; i++) {
+        // Risultati live
+        if ( (soste[i]["nome"].toUpperCase().replace(/\s+/g, " ").trim().includes(val.toUpperCase().replace(/\s+/g, " ").trim()) || ("0" + soste[i]["id"].trim()).slice(-2).includes(("0" + val.trim()).slice(-2)) ) && val != '') {
+            nada = false;
+            mostraRisultati('#listaSoste', 'soste', i, false);
+        }
+    }
+    // Risultati live
+    for (let i = 0; i < nominativi.length; i++) {
+        // str.replace(/\s+/g, " ").trim() -> remove space between words
+        if ( (nominativi[i]["nome"].toUpperCase().replace(/\s+/g, " ").trim().includes(val.toUpperCase().replace(/\s+/g, " ").trim()) || (nominativi[i]["nome"].toUpperCase().replace(/\s+/g, " ").trim().includes(val.toUpperCase().replace(/\s+/g, " ").trim().split(' ').slice(-1))) || ("0" + nominativi[i]["id"].trim()).slice(-2).includes(("0" + val.trim()).slice(-2)) ) && val != '') {
+            nada = false;
+            mostraRisultati('#listaSoste', 'nominativi', i, false);
+        }
+    }
+    if (val.trim().toLowerCase() == 'virgil') {
+        $('#listaSoste').append(`
+            <div class="card">
+                <div class="card-body text-center">
+                    <p class="display-2">ฅ^•ﻌ•^ฅ</p>
+                    <p class="h4 mb-0">BAVA VIRGIL</p>
+                    <p class="fs-6">Ora torna a lavoro!</p>
+                </div>
+            </div> 
+        `);
+    }
+    else if (nada && val != '') { // La ricerca non ha prodotto risultati :(
+        nessunRisultato('#listaSoste');
+    }
+    if (val.trim().toLowerCase() == 'charly') { // Giochino per papà
+        // da fare )
+    }
+}
+
+// Svuota Ricerca
+function svuotaRicerca() {
+    $('#nuovaRicerca').hide();
+    $('#pulisciRicerca').hide();
+    $('#risultato').hide();
+    $("#inputSoste").prop("value", '').prop("disabled", false);
+}
+
+// Capitalizza le prime lettere di ogni parola
+function titleCase(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // You do not need to check if i is larger than splitStr length, as your for does that for you
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    // Directly return the joined string
+    return splitStr.join(' '); 
 }
--- scripts/main.js
+++ scripts/main.js
@@ -10,91 +10,12 @@ var turno = []; // Dove sono di turno oggi
 var dataOggi = new Date();
 var ricercaFatta = '';
 
-// Runna il sito :)
+// Manda in esecuzione il tutto :)
 main();
 
 
 
-// function impostaSosta() {
-//     $('#turno').html(`
-//         <div class="carousel-item active">
-//             <span class="d-inline h2 mb-0" style="font-family: 'Rubik', sans-serif; font-weight: 500;">
-//                 ...
-//             </span>
-//         </div>
-//     `);
-//     applicaSosta();
-// }
 
-function applicaSosta() {
-    
-    var modal = new bootstrap.Modal(document.getElementById('impostaSosteModal'));
-    modal.toggle();
-    
-    $('#inputImpostaSoste').val('').focus();
-    $('#impostaListaSosta').html('');
-
-    // Prendere gli input da tastiera per la ricerca live
-    $('#inputImpostaSoste').bind('click keyup', function () {
-        // Prendi quello che scrivi in input -> $(this).val();
-        var val = $(this).val();
-
-        // Se il campo di ricerca non è vuoto fai vedere solo i risultati giusti
-        if (val!= '') {
-            $("#impostaListaSosta").html('');
-        }
-        // Altrimenti mostra la scelta di ricerca
-        else {
-            $("#impostaListaSosta").html('');
-        }
-        var k = 0;
-        var nada = true;
-        for (let i = 0; i < soste.length && k < 2; i++) {
-            // Risultati live
-            if (soste[i]["nome"].toUpperCase().trim().includes(val.toUpperCase().trim()) && val != '') {
-                
-                nada = false;
-                k +=1; 
-
-                $('#impostaListaSosta').append(`
-                    <div class="card" onclick="impostaApplicaSosta(${soste[i]["id"]}, '${soste[i]["nome"]}');">
-                        <div class="card-body">
-                            <div class="row">                    
-                                <div class="col-2">
-                                    ${soste[i]["id"]}
-                                </div>
-                                <div class="col-auto">
-                                    ${soste[i]["nome"]}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                `);
-            }
-        }
-        if (nada && val != '') { // La ricerca non ha prodotto risultati :(
-            $('#impostaListaSosta').append(`
-                <div class="card">
-                    <div class="card-body text-center">
-                        <p class="h5">La ricerca non ha prodotto alcun risultato</p>
-                    </div>
-                </div> 
-            `);
-        }
-    });  
-}
-// Imposta la sosta scelta
-function impostaApplicaSosta(id, nome) {
-    // console.log(id + " " + nome, " | ", soste[id - 1]["id"] + " " + soste[id - 1]["nome"] );
-    turno["id"] = soste[id - 1]["id"];
-    turno["nome"] = soste[id - 1]["nome"];
-    stampaCalendario();
-    
-    $('#nuovaRicerca').hide();
-    $('#risultato').hide();
-    $("#inputSoste").prop("value", '').prop("disabled", false);
-    $('#impostaSosteModal').modal('hide');
-}
 // Apri il campo di ricerca
 function apriRicerca() {
     $('#turno').slideUp();
@@ -102,6 +23,7 @@ function apriRicerca() {
     $('#chiudiRicerca').show();
     $('#listaSoste').slideDown();
     $('#nuovaRicerca').hide();
+    $('#pulisciRicerca').hide();
     $('#risultato').hide();
     $("#inputSoste").prop("value", ricercaFatta).prop("disabled", false).focus();
     $('footer').hide();
@@ -111,102 +33,24 @@ function apriRicerca() {
         $('#listaSoste').html('');
         $('#sceltaRicerca').slideDown();
     }
-
+    
     // Prendere gli input da tastiera per la ricerca live
     $('#inputSoste').bind('click keyup', function () {
         // Prendi quello che scrivi in input -> $(this).val();
 
         // Se il campo di ricerca non è vuoto fai vedere solo i risultati giusti
         if ($(this).val() != '') {
-            $("#listaSoste").html('');
             $('#sceltaRicerca').slideUp();
         }
         // Altrimenti mostra la scelta di ricerca
         else {
-            $("#listaSoste").html('');
             $('#sceltaRicerca').slideDown();
         }
-
+        
+        $('#listaSoste').html('');
         risultatiRicerca($(this).val());
         ricercaFatta = $(this).val();
-
     });
-    
-
-}
-// Sistema di ricerca
-function risultatiRicerca(val) {
-
-    var nada = true;
-
-    for (let i = 0; i < soste.length; i++) {
-        // Risultati live
-        if (soste[i]["nome"].toUpperCase().trim().includes(val.toUpperCase().trim()) && val != '') {
-
-            nada = false;
-
-            $('#listaSoste').append(`
-                <div class="card" onclick="dimmiChi(${soste[i]["id"]}, '${soste[i]["nome"]}');">
-                    <div class="card-body">
-                        <div class="row">                    
-                            <div class="col-2">
-                                ${soste[i]["id"]}
-                            </div>
-                            <div class="col-auto">
-                                ${soste[i]["nome"]}
-                            </div>
-                        </div>
-                    </div>
-                </div>      
-            `);
-        }
-    }
-    for (let i = 0; i < nominativi.length; i++) {
-        // Risultati live
-        if (nominativi[i]["nome"].toUpperCase().trim().includes(val.toUpperCase().trim()) && val != '') {
-
-            nada = false;
-
-            $('#listaSoste').append(`
-                <div class="card" onclick="dimmiDove(${nominativi[i]["id"]}, '${nominativi[i]["nome"]}');">
-                    <div class="card-body">\
-                        <div class="row">
-                            <div class="col-2">
-                                ${nominativi[i]["id"]}
-                            </div>
-                            <div class="col-auto">
-                                ${nominativi[i]["nome"]}
-                            </div>
-                        </div>
-                    </div>
-                </div>       
-            `);
-        }
-    }
-    if (val.trim().toLowerCase() == 'virgil') {
-        $('#listaSoste').append(`
-            <div class="card">
-                <div class="card-body text-center">
-                    <p class="display-2">ฅ^•ﻌ•^ฅ</p>
-                    <p class="h4 mb-0">BAVA VIRGIL</p>
-                    <p class="fs-6">Ora torna a lavoro!</p>
-                </div>
-            </div> 
-        `);
-    }
-    else if (nada && val != '') { // La ricerca non ha prodotto risultati :(
-        $('#listaSoste').append(`
-            <div class="card">
-                <div class="card-body text-center">
-                    <p class="display-2">(&#62;_&#60;)</p>
-                    <p class="h5">La ricerca non ha prodotto alcun risultato</p>
-                </div>
-            </div> 
-        `);
-    }
-    if (val.trim().toLowerCase() == 'charly') { // Giochino per papà
-        // da fare )
-    }
 }
 
 // Chiudi (Pulsante)
@@ -243,6 +87,8 @@ function chiudiRisultatiRicerca() {
     $('#listaSoste').slideUp('slow');
 
     $('#nuovaRicerca').show();
+    $('#pulisciRicerca').show();
+
     $('#risultato').show();
 
     $('footer').show();
@@ -251,57 +97,4 @@ function chiudiRisultatiRicerca() {
     $('#sceltaRicerca').slideUp();
 }
 
-// Mostra il nome di chi sta a quella sosta
-function dimmiChi(id, nome) { // Fanno riferimento alle informazioni della sosta dove voglio andare
 
-    chiudiRisultatiRicerca();
-    var idNominativo = 0;
-
-    // idTurno = 40 (fisso), il tunro["id"] cambia giorno per giorno e l'id è della sosta su cui ho cliccato
-    idNominativo = idTurno - (turno["id"] - id);
-    if (idNominativo > 95)
-        idNominativo -= 95;
-
-    // console.log(idTurno, ( turno["id"] - id ), idNominativo);
-
-    $("#inputSoste").prop("value", nome).prop("disabled", true);
-    document.getElementById("risultato").innerHTML = `
-        <div class="card">
-            <div class="card-body" >
-               ${nominativi[idNominativo - 1]["nome"]}
-            </div>
-        </div>
-    `;
-}
-// Mostra la sosta dove sta quel nominativo
-function dimmiDove(id, nome) {
-
-    chiudiRisultatiRicerca();
-    var idSoste = 0;
-
-    idSoste = turno["id"] - ( idTurno - id );
-    if ( idSoste < 1 )
-        idSoste += 95;
-
-    // console.log(idTurno, ( turno["id"] - id ), idSoste);
-
-    $("#inputSoste").prop("value", nome).prop("disabled", true);
-    document.getElementById("risultato").innerHTML = `
-        <div class="card">
-            <div class="card-body" >
-            ${soste[idSoste - 1]["nome"]}
-            </div>
-        </div>
-    `;
-}
-// Capitalizza le prime lettere di ogni parola
-function titleCase(str) {
-    var splitStr = str.toLowerCase().split(' ');
-    for (var i = 0; i < splitStr.length; i++) {
-        // You do not need to check if i is larger than splitStr length, as your for does that for you
-        // Assign it back to the array
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
-    }
-    // Directly return the joined string
-    return splitStr.join(' '); 
-}
